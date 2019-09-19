a952e27cc5ef229a4754357eb27cfde993bb7bf1
addo <forison6205@gmail.com>
2019-09-19 11:06:57 -0400

    added user sign up and login feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+	before_action :new_signup_params, if: :devise_controller?
+	
+	protected 
+	def new_signup_params
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:lastname, :firstname, :dob, :gender])
+	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+	
+	def is_signup_path?
+		return true if request.original_url.include?("users/sign_up")
+		false     
+  end
 end
