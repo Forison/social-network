a467787611dec210399ee05f86f4b092c7c2096e
addo <forison6205@gmail.com>
2019-09-26 04:42:08 -0400

    added refactored routes

--- app/controllers/usersaffairs_controller.rb
+++ app/controllers/usersaffairs_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+class UsersaffairsController < ApplicationController
 	before_action :authenticate_user!
 	
 	def index 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :usersaffairs, only: [:index, :show, :edit, :update]
   devise_for :users
   resources :posts, only: [:index,:create]
 end
