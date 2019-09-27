e7485985912eff6a77508374425789123aff0d67
addo <forison6205@gmail.com>
2019-09-27 06:08:15 -0400

    resolved stickler issues;again

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true.
 module ApplicationHelper
-	
-	def is_signup_path?
-		return true if request.original_url.include?("users/sign_up")
-		false     
+  def is_signup_path?
+    return true if request.original_url.include?("users/sign_up")
+    false
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true.
 class Post < ApplicationRecord
   validates :postcontent, presence: true
   belongs_to :author, class_name: "User", foreign_key: :user_id
