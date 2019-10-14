0144431943f5f58eb46952817b8f75f088ea03ca
addo <forison6205@gmail.com>
2019-10-14 06:34:16 -0400

    fixed linters error

--- spec/feature/comment_spec.rb
+++ spec/feature/comment_spec.rb
@@ -8,12 +8,7 @@ RSpec.feature 'Comment testing' do
     @auth = log_in @user
     @post = create(:post, user_id: @user.id)
   end
-  scenario 'auth user comments on a post' do
-    visit root_path
-    fill_in 'comment[commentary]', with: 'hello comment'
-    click_on 'send'
-    expect(page).to have_text('comment posted')
-  end
+
   scenario 'auth user comments on a post' do
     visit posts_path
     fill_in 'comment[commentary]', with: 'hello comment'
@@ -25,5 +20,11 @@ RSpec.feature 'Comment testing' do
     fill_in 'comment[commentary]', with: 'hello comment'
     click_on 'send'
     expect(page).to have_text('comment posted')
+  end
+  scenario 'User unlikes post' do
+    create(:like, user_id: @user.id, post_id: @post.id)
+    visit post_path(@post.id)
+    click_on 'unlike'
+    expect(page).to have_text('unliked post')
   end
 end
--- spec/feature/comment_two_spec.rb
+++ spec/feature/comment_two_spec.rb
@@ -2,21 +2,27 @@
 
 require 'rails_helper'
 
-RSpec.feature 'like' do
+RSpec.feature 'Comment testing' do
   before do
     @user = create(:user)
-    @auth_user = log_in @user
+    @auth = log_in @user
     @post = create(:post, user_id: @user.id)
   end
-  scenario 'User likes post on root path' do
-    visit root_path
+
+  scenario 'User likes post' do
+    visit posts_path
     click_on 'Like'
     expect(page).to have_text('post liked')
   end
-  scenario 'User unlikes post root path' do
+  scenario 'User unlikes post' do
     create(:like, user_id: @user.id, post_id: @post.id)
-    visit root_path
+    visit posts_path
     click_on 'unlike'
     expect(page).to have_text('unliked post')
+  end
+  scenario 'User likes post' do
+    visit post_path(@post.id)
+    click_on 'Like'
+    expect(page).to have_text('post liked')
   end
 end
